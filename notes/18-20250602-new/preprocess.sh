88e0c9da855804942e2c23ae739d21bb16f459b3
Sumin Park <41722206+suparklingmin@users.noreply.github.com>
2025-06-02 07:44:09 +0000

    preprocess 업데이트

--- notes/18-20250602-new/preprocess.sh
+++ notes/18-20250602-new/preprocess.sh
@@ -1,3 +1,5 @@
 wget https://www.gutenberg.org/cache/epub/29728/pg29728.txt -O apicius.txt
 
 egrep -o "[A-Z][A-Z]+" apicius.txt | sort | uniq -c | sort -nr > word-freq.txt
+
+head -30 word-freq.txt | awk -f txt-to-json.awk > data.json

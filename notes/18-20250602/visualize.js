4835c96b04d07909888d05ab3a9bedc21c1efce8
Sumin Park <41722206+suparklingmin@users.noreply.github.com>
2025-06-02 14:12:44 +0900

    Add files via upload

--- notes/18-20250602/visualize.js
+++ notes/18-20250602/visualize.js
@@ -1,6 +1,6 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
-fetch('./data.json')
+fetch('https://raw.githubusercontent.com/suparklingmin/IntroDSH2025/refs/heads/main/notes/18-20250602/data.json')
 .then(response => response.json())
 .then(chartData => {
     const chart = new Chart(ctx, {
